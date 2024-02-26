ce22ef64eb4e09413a125c72955d9010491055f3
tehnickge <71207932+tehnickge@users.noreply.github.com>
2024-02-26 15:03:27 +0300

    add card and container.scss

---
+++ cute-catz/src/components/Card/Card.tsx
@@ -0,0 +1,28 @@
+import styles from "./Card.module.scss"
+
+
+
+export default function Card() {
+
+
+
+    return (
+        <div className={styles.card}>
+            <div className="card__image">
+
+            </div>
+            <ul className={styles.test}>
+                <li><a href="#" className="">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+            </ul>
+        </div>
+    )
+}
